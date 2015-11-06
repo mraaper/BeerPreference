16876ac59a1c4c3163c8d6bf6dd6b59f1644818a
mraaper <madis.raaper@itcollege.ee>
2015-11-06 19:27:24 +0200

    On arenguid

--- src/BeerPreference/BeerAnswers.java
+++ src/BeerPreference/BeerAnswers.java
@@ -1,9 +1,14 @@
 package BeerPreference;
 
-public class BeerGeneral {
+import java.util.ArrayList;
+
+import lib.TextIO;
+
+public class BeerAnswers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 		
 		// public class - KÜSITLUS
 		
@@ -13,6 +18,7 @@ public class BeerGeneral {
 		
 		// KÜSITLUSE class
 		// meetod - küsitluse vorm
+		// küsitluse alla kuuluvad: nimi, vanus (kui > 18, siis OK, kui < 18, siis NÕUP)
 		// TICKID - õllevalikute jne kohta, ei ole eraldi küsimus-vastus tekstina
 		// USER INPUT meetod - võimaldab kasutajal vastata ja salvestab vastused muutujatesse
 		// CHANGES THE QUESTION meetod
---
+++ src/BeerPreference/BeerQuestionnaire.java
@@ -0,0 +1,21 @@
+package BeerPreference;
+
+public class BeerQuestionnaire {
+	
+	public String name;
+	public int age;
+	
+	
+		
+		
+		
+		
+		
+		String kysimus1 = ("Mis on Teie nimi, härra/preili?");
+		
+		
+	
+		
+	}
+
+}
