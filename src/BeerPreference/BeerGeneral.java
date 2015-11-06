d29aa9ce8b458e3bbf93842ce49736e23d83156d
mraaper <madis.raaper@itcollege.ee>
2015-11-06 17:50:38 +0200

    Algus

--- src/BeerPreference/BeerGeneral.java
+++ src/BeerPreference/BeerGeneral.java
@@ -4,6 +4,32 @@ public class BeerGeneral {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// public class - KÜSITLUS
+		
+		// public class - VASTUSTE TÖÖTLUS
+		
+		// public class - EELISTUS
+		
+		// KÜSITLUSE class
+		// meetod - küsitluse vorm
+		// TICKID - õllevalikute jne kohta, ei ole eraldi küsimus-vastus tekstina
+		// USER INPUT meetod - võimaldab kasutajal vastata ja salvestab vastused muutujatesse
+		// CHANGES THE QUESTION meetod
+		
+		// VASTUSTE TÖÖTLUSE class 
+		// vastused võetakse muutujatena sisse ja töötleb neid
+		// eeljaotus - millised õlle vastavad kindlatele kriteeriumitele
+		// leiab kasutajale sobiva õlle - leiab sobiva rühma
+		
+		// EELISTUSE class 
+		// annab vastuse lähtudes eelistustest
+		// pildi loomine koos tekstiga
+		// üks meetod sisaldab pilte ja teine filtreerib pilte
+		// lõpuks kasutaja arvamus
+		
+		// nurgas all peab olema nimi ja rühma number
+		
 
 	}
 
