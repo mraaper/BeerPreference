519177585b6b1edb253f22e35a96351c877b2c49
mraaper <madis.raaper@itcollege.ee>
2015-11-08 21:23:51 +0200

    Küsitluse välja loomine

--- src/BeerPreference/BeerQuestionnaire.java
+++ src/BeerPreference/BeerQuestionnaire.java
@@ -1,21 +1,47 @@
 package BeerPreference;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
-public class BeerQuestionnaire {
+public class BeerQuestionnaire extends Application{
 	
-	public String name;
-	public int age;
-	
-	
-		
-		
-		
-		
-		
+	//Osad koodiread baseeruvad Krister Viirsaare Java koduleheküljel http://i200.itcollege.ee/javafx
+    	
 		String kysimus1 = ("Mis on Teie nimi, härra/preili?");
+		String kysimus2 = ("Palun sisestage oma vanus");
+		@Override
+		public void start(Stage primaryStage) throws Exception {
+			
+		primaryStage.setTitle("Õllede küsimustik");
+		VBox vbox = new VBox();
+		vbox.setSpacing(7);
+        Scene scene = new Scene(vbox, 100, 100);
+        primaryStage.setScene(scene);
+        Label kys1 = new Label();
+        kys1.setText("Kysimuse sisu");
+        kys1.setTranslateX(0);
+        kys1.setTranslateY(0);
+        ToggleGroup kysimus1 = new ToggleGroup();
+        RadioButton k1 = new RadioButton("Variant 1");
+        RadioButton k2 = new RadioButton("Variant 2");
+        RadioButton k3 = new RadioButton("Variant 3");
+        k1.setToggleGroup(kysimus1);
+        k2.setToggleGroup(kysimus1);
+        k3.setToggleGroup(kysimus1);
+        k3.setSelected(true);
+        vbox.getChildren().addAll(kys1, k1, k2, k3);
+        primaryStage.show();	
+		}
 		
 		
 	
 		
 	}
 
-}
+
