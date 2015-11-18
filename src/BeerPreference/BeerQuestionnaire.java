c7e1d85674a6ebec93e560092bb2f5cea4b92e86
mraaper <madis.raaper@itcollege.ee>
2015-11-18 21:24:22 +0200

    Töö jätkub

--- src/BeerPreference/BeerQuestionnaire.java
+++ src/BeerPreference/BeerQuestionnaire.java
@@ -14,23 +14,29 @@ public class BeerQuestionnaire extends Application{
 	//Osad koodiread baseeruvad Krister Viirsaare Java koduleheküljel http://i200.itcollege.ee/javafx
     	
 		String kysimus1 = ("Mis on Teie nimi, härra/preili?");
-		String kysimus2 = ("Palun sisestage oma vanus");
+		String kysimus2 = ("Palun märkige oma vanus");
+		String kysimus3 = ("Palun valige enda maitse järgi õlule sobiv aroom");
+		String kysimus4 = ("Palun valige sobiv värvus");
+		String kysimus5 = ("Palun valige sobiv tugevus");
+		String kysimus6 = ("Palun valige sobiv maitse");
+		String kysimus7 = ("Palun valige sobiv tüüp");
+		String kysimus8 = ("Palun valige sobiv piirkond");
 		@Override
 		public void start(Stage primaryStage) throws Exception {
 			
 		primaryStage.setTitle("Õllede küsimustik");
 		VBox vbox = new VBox();
-		vbox.setSpacing(7);
-        Scene scene = new Scene(vbox, 100, 100);
+		vbox.setSpacing(17);
+        Scene scene = new Scene(vbox, 200, 175);
         primaryStage.setScene(scene);
         Label kys1 = new Label();
-        kys1.setText("Kysimuse sisu");
-        kys1.setTranslateX(0);
-        kys1.setTranslateY(0);
+        kys1.setText("Palun märkige oma vanus");
+        kys1.setTranslateX(20);
+        kys1.setTranslateY(5);
         ToggleGroup kysimus1 = new ToggleGroup();
-        RadioButton k1 = new RadioButton("Variant 1");
-        RadioButton k2 = new RadioButton("Variant 2");
-        RadioButton k3 = new RadioButton("Variant 3");
+        RadioButton k1 = new RadioButton("18-30");
+        RadioButton k2 = new RadioButton("31-49");
+        RadioButton k3 = new RadioButton("50-75");
         k1.setToggleGroup(kysimus1);
         k2.setToggleGroup(kysimus1);
         k3.setToggleGroup(kysimus1);
