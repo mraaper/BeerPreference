13a997a365f990bfc8bf231b85b96952a5a419f8
mraaper <madis.raaper@itcollege.ee>
2015-12-06 15:18:40 +0200

    Kood töötab, aga vaid kahes klassis. Quo vadis, kallis Java koodike?!

--- src/BeerPreference/BeerAnswers.java
+++ src/BeerPreference/BeerAnswers.java
@@ -1,41 +1,12 @@
 package BeerPreference;
 
-import java.util.ArrayList;
-
-import lib.TextIO;
-
 public class BeerAnswers {
 
 	public static void main(String[] args) {
-		new Questionnaire();
+		new Beer();
 		
 		
 		
-		// public class - KÜSITLUS
-		
-		// public class - VASTUSTE TÖÖTLUS
-		
-		// public class - EELISTUS
-		
-		// KÜSITLUSE class
-		// meetod - küsitluse vorm
-		// küsitluse alla kuuluvad: nimi, vanus (kui > 18, siis OK, kui < 18, siis NÕUP)
-		// TICKID - õllevalikute jne kohta, ei ole eraldi küsimus-vastus tekstina
-		// USER INPUT meetod - võimaldab kasutajal vastata ja salvestab vastused muutujatesse
-		// CHANGES THE QUESTION meetod
-		
-		// VASTUSTE TÖÖTLUSE class 
-		// vastused võetakse muutujatena sisse ja töötleb neid
-		// eeljaotus - millised õlle vastavad kindlatele kriteeriumitele
-		// leiab kasutajale sobiva õlle - leiab sobiva rühma
-		
-		// EELISTUSE class 
-		// annab vastuse lähtudes eelistustest
-		// pildi loomine koos tekstiga
-		// üks meetod sisaldab pilte ja teine filtreerib pilte
-		// lõpuks kasutaja arvamus
-		
-		// nurgas all peab olema nimi ja rühma number
 		
 
 	}
--- src/BeerPreference/BeerPreference.java
+++ src/BeerPreference/BeerPreference.java
@@ -1,6 +1,7 @@
 package BeerPreference;
 
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,16 +13,15 @@ import java.util.Enumeration;
 // Koodiread võtsin eeskujuks, mille alusel koostasin enda user inputi ja vastavad vahemikud
 
 class Beer extends JFrame implements ActionListener {
-	JPanel panel;
+	JPanel background;
 	JRadioButton choice1;
 	JRadioButton choice2;
 	JRadioButton choice3;
-	ButtonGroup bg;
-	JLabel lblmess;
-	JButton btnext;
-	String[][] qpa;
-	String[][] questionAnswers;
-	int qaid;
+	ButtonGroup buttonSelection;
+	JLabel setLabel;
+	JButton nextButton;
+	String[][] beerQuestion;
+	int questionAmount;
 
 	int pointCollection = 0;
 
@@ -32,104 +32,103 @@ class Beer extends JFrame implements ActionListener {
 		setSize(430, 350);
 		setLocation(300, 100);
 		setResizable(false);
-		Container cont = getContentPane();
-		cont.setLayout(null);
-		cont.setBackground(Color.BLACK);
-		bg = new ButtonGroup();
+		Container setScene = getContentPane();
+		setScene.setLayout(null);
+		setScene.setBackground(Color.BLACK);
+		buttonSelection = new ButtonGroup();
 		choice1 = new JRadioButton("Choice1", true);
 		choice2 = new JRadioButton("Choice2", true);
 		choice3 = new JRadioButton("Choice3", true);
 
-		bg.add(choice1);
-		bg.add(choice2);
-		bg.add(choice3);
+		buttonSelection.add(choice1);
+		buttonSelection.add(choice2);
+		buttonSelection.add(choice3);
 
-		lblmess = new JLabel("Anna oma sisend");
-		lblmess.setForeground(Color.BLACK);
-		lblmess.setFont(new Font("Berlin Sans FB", Font.BOLD, 15));
-		btnext = new JButton("Next");
-		btnext.setForeground(Color.BLACK);
-		btnext.addActionListener(this);
-		panel = new JPanel();
-		panel.setBackground(Color.LIGHT_GRAY);
-		panel.setLocation(10, 10);
-		panel.setSize(400, 300);
-		panel.setLayout(new GridLayout(6, 2));
-		panel.add(lblmess);
-		panel.add(choice1);
-		panel.add(choice2);
-		panel.add(choice3);
+		setLabel = new JLabel();
+		setLabel.setForeground(Color.BLACK);
+		setLabel.setFont(new Font("Berlin Sans FB", Font.BOLD, 15));
+		nextButton = new JButton("Next");
+		nextButton.setForeground(Color.BLACK);
+		nextButton.addActionListener(this);
+		background = new JPanel();
+		background.setBackground(Color.LIGHT_GRAY);
+		background.setLocation(10, 10);
+		background.setSize(400, 300);
+		background.setLayout(new GridLayout(6, 2));
+		background.add(setLabel);
+		background.add(choice1);
+		background.add(choice2);
+		background.add(choice3);
 
-		panel.add(btnext);
-		cont.add(panel);
+		background.add(nextButton);
+		setScene.add(background);
 		setVisible(true);
-		qaid = 0;
-		readqa(qaid);
+		questionAmount = 0;
+		calculate(questionAmount);
 
 	}
 
 	public void actionPerformed(ActionEvent e) {
 
-		if (btnext.getText().equals("Next")) {
-			if (qaid < 6) {
+		if (nextButton.getText().equals("Next")) {
+			if (questionAmount < 6) {
 
 				pointCollection = pointCollection + getPoints();
-				qaid++;
-				readqa(qaid);
+				questionAmount++;
+				calculate(questionAmount);
 			} else {
 				pointCollection = pointCollection + getPoints();
-				btnext.setText("Ihaldusobjektiks on ...");
+				nextButton.setText("Ihaldusobjektiks on ...");
 
 			}
-		} else if (btnext.getText().equals("Ihaldusobjektiks on ..."))
+		} else if (nextButton.getText().equals("Ihaldusobjektiks on ..."))
 			new Report();
 
 	}
 
 	public void initializedata() {
-		// qpa stores pairs of question and its possible answers
-		qpa = new String[10][5];
+		beerQuestion = new String[10][5];
 
-		qpa[0][0] = "Palun märkige oma vanus";
-		qpa[0][1] = "18-30";
-		qpa[0][2] = "31-49";
-		qpa[0][3] = "50-75";
+		beerQuestion[0][0] = "Palun märkige oma vanus";
+		beerQuestion[0][1] = "18-30";
+		beerQuestion[0][2] = "31-49";
+		beerQuestion[0][3] = "50-75";
 
-		qpa[1][0] = "Palun valige enda maitse järgi õlule sobiv aroom";
-		qpa[1][1] = "Lõhnatu on mu teine nimi";
-		qpa[1][2] = "Parajalt humalane ja hää";
-		qpa[1][3] = "Külmade talveõhtute nauding";
+		beerQuestion[1][0] = "Palun valige enda maitse järgi õlule sobiv aroom";
+		beerQuestion[1][1] = "Lõhnatu on mu teine nimi";
+		beerQuestion[1][2] = "Parajalt humalane ja hää";
+		beerQuestion[1][3] = "Külmade talveõhtute nauding";
 
-		qpa[2][0] = "Palun valige õlule sobiv värvus";
-		qpa[2][1] = "Naeratav blondiin";
-		qpa[2][2] = "Õhetav punapea";
-		qpa[2][3] = "Naudisklev brünett";
+		beerQuestion[2][0] = "Palun valige õlule sobiv värvus";
+		beerQuestion[2][1] = "Naeratav blondiin";
+		beerQuestion[2][2] = "Õhetav punapea";
+		beerQuestion[2][3] = "Naudisklev brünett";
 
-		qpa[3][0] = "Palun valige õllele sobiv tugevus";
-		qpa[3][1] = "Kärbes/sulgkaal";
-		qpa[3][2] = "Isa soovitusel midagi hääd!";
-		qpa[3][3] = "Sink the Bismarck!";
+		beerQuestion[3][0] = "Palun valige õllele sobiv tugevus";
+		beerQuestion[3][1] = "Kärbes/sulgkaal";
+		beerQuestion[3][2] = "Isa soovitusel midagi hääd!";
+		beerQuestion[3][3] = "Sink the Bismarck!";
 
-		qpa[4][0] = "Palun valige õllele sobiv maitse";
-		qpa[4][1] = "Oot...see maitseb ju nagu....vesi?!";
-		qpa[4][2] = "Päris hea humalane ettevõtmine";
-		qpa[4][3] = "Oleme me kindlad, et tegu on lahja alkoholiga??";
+		beerQuestion[4][0] = "Palun valige õllele sobiv maitse";
+		beerQuestion[4][1] = "Oot...see maitseb ju nagu....vesi?!";
+		beerQuestion[4][2] = "Päris hea humalane ettevõtmine";
+		beerQuestion[4][3] = "Oleme me kindlad, et tegu on lahja alkoholiga??";
 
-		qpa[5][0] = "Palun valige õllele sobiv tüüp";
-		qpa[5][1] = "Malbe ja tagasihoidlik";
-		qpa[5][2] = "Põhimõtteliselt olen valmis seikluseks, vist";
-		qpa[5][3] = "Don't care, don't want to know!";
+		beerQuestion[5][0] = "Palun valige õllele sobiv tüüp";
+		beerQuestion[5][1] = "Malbe ja tagasihoidlik";
+		beerQuestion[5][2] = "Põhimõtteliselt olen valmis seikluseks, vist";
+		beerQuestion[5][3] = "Don't care, don't want to know!";
 
-		qpa[6][0] = "Palun valige õlle päritolu piirkond";
-		qpa[6][1] = "A mul on jummala ükskõik ää";
-		qpa[6][2] = "Euroopast saab hääd kraami";
-		qpa[6][3] = "EESTI!";
+		beerQuestion[6][0] = "Palun valige õlle päritolu piirkond";
+		beerQuestion[6][1] = "A mul on jummala ükskõik ää";
+		beerQuestion[6][2] = "Euroopast saab hääd kraami";
+		beerQuestion[6][3] = "EESTI!";
 
 	}
 
 	public int getPoints() {
 		int points = 0;
-		Enumeration<AbstractButton> buttons = bg.getElements();
+		Enumeration<AbstractButton> buttons = buttonSelection.getElements();
 		while (buttons.hasMoreElements()) {
 			JRadioButton temp = (JRadioButton) buttons.nextElement();
 			points = points + 1;
@@ -141,26 +140,26 @@ class Beer extends JFrame implements ActionListener {
 		return points;
 	}
 
-	public void readqa(int qid) {
-		lblmess.setText("  " + qpa[qid][0]);
-		choice1.setText(qpa[qid][1]);
-		choice2.setText(qpa[qid][2]);
-		choice3.setText(qpa[qid][3]);
+	public void calculate(int score) {
+		setLabel.setText("  " + beerQuestion[score][0]);
+		choice1.setText(beerQuestion[score][1]);
+		choice2.setText(beerQuestion[score][2]);
+		choice3.setText(beerQuestion[score][3]);
 
 		choice1.setSelected(true);
 	}
 
 	public void reset() {
-		qaid = 0;
+		questionAmount = 0;
 
-		readqa(qaid);
-		btnext.setText("Next");
+		calculate(questionAmount);
+		nextButton.setText("Next");
 	}
 
 	public class Report extends JFrame {
 		Report() {
 			setTitle("Õllesoovitus");
-			setSize(900, 250);
+			setSize(1200, 250);
 			setBackground(Color.WHITE);
 			addWindowListener(new WindowAdapter() {
 				public void windowClosing(WindowEvent e) {
@@ -175,20 +174,20 @@ class Beer extends JFrame implements ActionListener {
 
 		class Draw extends Canvas {
 			public void paint(Graphics g) {
-				
+
 				int x = 150;
 				int y = 120;
 
 				g.setFont(new Font("Arial", Font.BOLD, 36));
 
 				if (pointCollection < 10) {
-					g.drawString("Soovitus: Lindemans Kriek  3,5%", x, y);
+					g.drawString("Madis Raaper (IA17) soovitab: Lindemans Kriek  3,5%", x, y);
 				} else if (pointCollection >= 10 && pointCollection <= 13) {
-					g.drawString("Soovitus: Brewdog Punk IPA  5,6%", x, y);
+					g.drawString("Madis Raaper (IA17) soovitab: Brewdog Punk IPA  5,6%", x, y);
 				} else if (pointCollection >= 14 && pointCollection <= 17) {
-					g.drawString("Soovitus: Rochefort Trappistes 8  9,2%", x, y);
+					g.drawString("Madis Raaper (IA17) soovitab: Rochefort Trappistes 8  9,2%", x, y);
 				} else {
-					g.drawString("Soovitus: Boneyard Baltic Notorious IPA3  12%", x, y);
+					g.drawString("Madis Raaper (IA17) soovitab: Boneyard Baltic Notorious IPA3  12%", x, y);
 				}
 
 			}
@@ -198,16 +197,7 @@ class Beer extends JFrame implements ActionListener {
 
 }
 
-public class BeerPreference {
-
-	public static void main(String args[]) {
-		new Beer();
-
-	}
-
-}
-
-// Iga küsimuse vastamise korral on väljendub sisend punktides.
+// Iga küsimuse vastamise korral väljendub sisend punktides.
 // Sõltuvalt vastustest ja akumuleerunud punktidest tuleneb ka õllesoovitus.
 // Vahemikke on 4: 0-9p, 10p-13p, 14-17p, 18-21p.
 // I kategooria soovitus: Lindemans Kriek 3,5%
